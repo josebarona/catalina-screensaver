577b096c5f2244c4e793bbad832a99d0d48091bd
josebarona <jose.barona@ing.austral.edu.ar>
2020-11-11 00:14:34 -0500

    fixed indentation, minor refactor

--- public/sketch.js
+++ public/sketch.js
@@ -1,42 +1,30 @@
-//code from: https://varun.ca/vector-field/
-
-var locs = [];
+const arr = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  var res = 20;
-  var countX = ceil(width/res) + 1;
-  var countY = ceil(height/res) + 1;
-
-  for (var j = 0; j < countY; j++) {
-    for (var i = 0; i < countX; i++) {
-      locs.push( new p5.Vector(res*i, res*j) );
+    createCanvas(800, 800);
+    
+    const res = 20;
+    
+    for (let j = 0; j < ceil(width / res) + 1; j++) {
+        for (let i = 0; i < ceil(height / res) + 1; i++) {
+            const vect = new p5.Vector(res * i, res * j);
+            const col = color(random(255), random(255), random(255));
+            arr.push({vect, col});
+        }
     }
-  };
-
-  noFill();
-  stroke(249,78,128);
+    
+    noFill();
 }
 
 function draw() {
-  background(30,67,137);
-  for (var i = locs.length - 1; i >= 0; i--) {
-    var h = calcVec( locs[i].x - mouseX, locs[i].y - mouseY);
-    line(
-      locs[i].x, 
-      locs[i].y, 
-      locs[i].x + 15*cos(h.heading()), 
-      locs[i].y + 15*sin(h.heading())
-    );
-  };
+    background(30, 67, 137);
+    arr.forEach(curr => {
+        const {x, y} = curr.vect;
+        const h = f(x - mouseX, y - mouseY);
+        const [x1, y1] = [x + 15 * cos(h.heading()), y + 15 * sin(h.heading())];
+        stroke(curr.col);
+        line(x, y, x1, y1);
+    });
 }
 
-function calcVec(x, y) {
-  return new p5.Vector(y - x, - x - y);
-}
-
-
-
-
-Resources
+const f = (x, y) => new p5.Vector(y - x, - x - y);
